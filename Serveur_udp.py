009ac9a998b219710538f23c0f76e6876908f2bb
Yann <abalainy@lacroix-rouge.fr>
2017-10-17 11:00:13 +0200

    ajout de la longitude avec resultat attendu

--- Serveur_udp.py
+++ Serveur_udp.py
@@ -1,12 +1,12 @@
-# -*-coding:Latin-1 -*
 #!/usr/bin/env python
+# -*-coding:Latin-1 -*
 # _*_ coding: utf-8 _*_
 
 import codecs
 import socket
 
-IP_Serv = "192.168.0.221" #IP Raspi
-#IP_Serv = "127.0.0.1"
+#IP_Serv = "192.168.0.221" #IP Raspi
+IP_Serv = "127.0.0.1"
 port = 8888
 
 ID = 65
@@ -19,27 +19,15 @@ sock= socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind ((IP_Serv, port))
 
 while True:
-
-    data,addr = sock.recvfrom (13) #ligne de décodage des trames.
+    print "_______________________________________________________"
+    data,addr = sock.recvfrom (1024) #ligne de dÃ©codage des trames.
     print "ID du systeme", ord (data[0])
     print "Taille de la trame", ord (data[1])
     print "Position de la GV", ord (data[2])
     print "Position du safran", ord (data[3])
 
-    trame = bytearray([ID, TAILLE, VVENT, DVENT, GITE]) #bytearray creer un tableau.
-            
-    sock.sendto(trame_reponse, addr)
-
-
-    print ""
-    print ""
-    print "marche pas"
-
-
-
-
     lat1 = 1651782
-    #lon1 = 21.0122287
+    lon1 = 210122287
     #lat2 = 52.406374
     #lon2 = 16.9251681
 
@@ -48,11 +36,25 @@ while True:
     b1=(lat1>>8)&0xFF
     b0=(lat1>>0)&0xFF
 
+    b4=(lon1>>24)
+    b5=(lon1>>16)&0xFF
+    b6=(lon1>>8)&0xFF
+    b7=(lon1>>0)&0xFF
+
     print ""
     print ""
     print "latittude", lat1
-    print " Vakeur Hexa de b2 : ", hex(b2)
-    print " Vakeur Hexa de b1 : ", hex(b1)
-    print " Vakeur Hexa de bo : ", hex(b0)
+    print " Valeur Hexa de b3 : ", hex(b3) 
+    print " Valeur Hexa de b2 : ", hex(b2)
+    print " Valeur Hexa de b1 : ", hex(b1)
+    print " Valeur Hexa de bo : ", hex(b0)
+
+    trame_reponse = bytearray([ID, TAILLE, VVENT, DVENT, GITE, b3,b2,b1,b0,b4,b5,b6,b7]) #bytearray creer un tableau.
+            
+    sock.sendto(trame_reponse, addr)
+
+
+
+
 
 
--- clientdemande.py
+++ clientdemande.py
@@ -3,9 +3,15 @@
 
 import socket
 
-IP_Dest = "192.168.0.221" #Addresse du serveur
+#IP_Dest = "192.168.0.221" #Addresse du serveur
+IP_Dest = "127.0.0.1" #Addresse du serveur
 port = 8888 #Port du serveur
-trame = bytearray([22,2,30,90])
+idTrame=22
+lgData=2
+safran=30
+gV=90
+trame = bytearray([idTrame,lgData,safran,gV])
+
 
 connexion_serveur = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 #socket.AF_DGRAM = Internet
@@ -21,13 +27,44 @@ print 'Message envoyé: ', trame[0],trame[1],trame[2],trame[3]
 
 connexion_serveur.sendto (trame, (IP_Dest, port)) #Envoie du message au serveur
 
-data, addresse = connexion_serveur.recvfrom(4)
+data, addresse = connexion_serveur.recvfrom(1024)
+
+
+lat0=((ord(data[5])<<24))
+lat1=((ord(data[6])<<16))
+lat2=((ord(data[7])<<8))
+lat3=((ord(data[8])<<0))
+
+if lat3 > 127:
+	latitude=(~latitude)&0xFFFFFFFF
+	latitude=latitude+1
+	latitude=latitude*-1
+
+lat=lat0|lat1|lat2|lat3
+
+lon0=((ord(data[9])<<24))
+lon1=((ord(data[10])<<16))
+lon2=((ord(data[11])<<8))
+lon3=((ord(data[12])<<0))
+
+if lon3 > 127:
+	longitude=(~longitude)&0xFFFFFFFF
+	longitude=longitude+1
+	longitude=longitude*-1
+
+lon=lon0|lon1|lon2|lon3
+
 
 print '------------------------'
 
 print 'Réponse du serveur:'
 
 print 'ID: ', ord(data[0])
-print 'Taille: ', ord(data[1])
-print 'Safran: ',ord(data[2])
-print 'GV: ',ord(data[2])
+print 'Taille: ', ord(data[1]),' octets'
+print 'vitVent: ', ord(data[2])
+print 'dirVent: ', ord(data[3])
+print 'lattitude: ', ord(data[5]), ord(data[6]), ord(data[7]), ord(data[8])
+print 'latitude.hex', hex(ord(data[5])),hex(ord(data[6])),hex(ord(data[7])),hex(ord(data[8]))
+print 'latitude ', lat
+print 'longitude: ',lon
+print 'gite: ',ord(data[4])
